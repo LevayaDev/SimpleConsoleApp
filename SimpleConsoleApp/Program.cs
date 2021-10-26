b7755b8bf3070932292189336b525964e4479681
Анастасия Жукова <levayadev@outlook.com>
2021-10-26 12:21:15 +0300

    1

--- SimpleConsoleApp/Program.cs
+++ SimpleConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace SimpleConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Everyone!");
         }
     }
 }
